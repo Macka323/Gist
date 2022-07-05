154f49764f3e8862bf5946496e41b5ecd7936464
Martin Milchevski <75259519+Macka323@users.noreply.github.com>
2022-07-05 23:48:11 +0200

    removed winget

--- setup.ps1
+++ setup.ps1
@@ -17,9 +17,7 @@ mkdir c:\macka32
 Add-MpPreference -ExclusionPath "C:\macka32"
 Add-MpPreference -ExclusionPath "C:\macka32\KMS\KMSTools.exe"
 
-Invoke-WebRequest https://github.com/microsoft/winget-cli/releases/download/v1.1.12653/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle -o c:\macka32\winget.msixbundle
 Invoke-WebRequest https://github.com/Macka323/Gist/raw/main/KMS.zip -o c:\macka32\KMS.zip
 Expand-Archive -LiteralPath "C:\macka32\KMS.zip" -DestinationPath "C:\macka32\KMS" -Force
-start c:\macka32\winget.msixbundle
 start c:\macka32\KMS\KMSTools.exe
 Timeout /T 200
